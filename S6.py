ef4720d11fd421046efa8e7276842f9e1000d47e
lavindude <lavindude@gmail.com>
2020-11-18 19:41:16 -0800

    S6 done

--- S6.py
+++ S6.py
@@ -1,5 +1,10 @@
 '''
-Problem 1: Quadratic Formula calculator
+Problem 1: Quadratic Formula calculator (inputs are the coefficients of the quadratic
+in standard form)
+
+Directions: Make a function that takes in a, b, and c from the standard form of a 
+            quadratic and provides the real solutions of the quadratic.
+
 '''
 
 import math
@@ -7,29 +12,38 @@ import math
 times = int(input("How many quadratic equations would you like to calculate? "))
 
 def quadratic(a, b, c):
-    #Solution 1:
-    numerator = -1 * b + math.sqrt(b ** 2 - 4 * a * c)
-    denom = 2 * a
-    sol1 = numerator / denom
+    try:
+        #Solution 1:
+        numerator = -1 * b + math.sqrt(b ** 2 - 4 * a * c)
+        denom = 2 * a
+        sol1 = numerator / denom
+        
+        #Solution 2:
+        numerator = -1 * b - math.sqrt(b ** 2 - 4 * a * c)
+        sol2 = numerator / denom
 
-    #Solution 2:
-    numerator = -1 * b - math.sqrt(b ** 2 - 4 * a * c)
-    sol2 = numerator / denom
+        if sol1 == sol2:
+            print("The solution is x = " + str(sol1))
 
-    print("Solutions are x=" + str(sol1) + " and x=" + str(sol2))
+        else:
+            print("Solutions are x = " + str(sol1) + " and x = " + str(sol2))
+
+    except: 
+        print("No real solutions.")
+
     print()
 
 for i in range(0, times):
-    a = int(input("Enter the coefficient of x^2: "))
-    b = int(input("Enter the coefficient of x: "))
-    c = int(input("Enter the constant (c): "))
+    a = float(input("Enter the coefficient of x^2: "))
+    b = float(input("Enter the coefficient of x: "))
+    c = float(input("Enter the constant (c): "))
     print()
 
     quadratic(a, b, c)
 
 '''
 Problem 2 Make a function that gets a list from a user and orders it from least to greatest.
-Do not use the .sort() function.
+Do not use the .sort() function. 
 '''
 
 def least_to_greatest(og):
